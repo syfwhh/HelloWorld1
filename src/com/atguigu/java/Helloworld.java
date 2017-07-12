938ec31853fab8d1cc0a89f0c79e846856d42819
lisi <yuanfang0217@126.com>
2017-07-12 17:33:34 +0800

    update

--- src/com/atguigu/java/Helloworld.java
+++ src/com/atguigu/java/Helloworld.java
@@ -4,5 +4,8 @@ public class Helloworld {
 	
 	public static void main(String[] args) {
 		System.out.println("你好");
+		System.out.println("你好");
+		System.out.println("12");
+		System.out.println("123");
 	}
 }
